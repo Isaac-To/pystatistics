42d72a2a826d6ffc0dec225cf2fc4aa079e1dbc7
Isaac To <77095583+chisaku-dev@users.noreply.github.com>
2021-09-12 23:07:35 -0700

    Setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+    name = 'purePyStatistics',
+    packages = ['pystatistics'],
+    version = '0.1.4',
+    license='MIT',
+    description = 'Basic Functions for Statistics',
+    author = 'Isaac To',
+    url = 'https://github.com/chisaku-dev/pyDataStats',
+    download_url = 'https://github.com/chisaku-dev/pyDataStats/archive/refs/tags/0.1.tar.gz',
+    keywords = ['statistics', 'data'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
